1541d7aeedcee6f680a8f0c1615612ef8fdaa697
Eron Lake <ejlake@ucsc.edu>
2015-03-01 19:47:25 -0800

    the menu

--- menu/menu.js
+++ menu/menu.js
@@ -4,14 +4,97 @@ var context = canvas.getContext('2d');
  
  //---------------------------------------------------------------
  var bag = [];
+ //---------------------------------------------------------------
  
+ 
+ 
+ 
+ 
+ 
+//////////////////////////////////////////////////////////////////
+//List of Weapons
+//////////////////////////////////////////////////////////////////
+
+ 
+//---------------------------------------------------------------------------
+
  var sword = {first: 'sword', second: true, 
  			run: function(){
  				bag.push({name: 'sword', health: 0, integrity: 0, attack: 5});
  			}
  	};
+ 	
+//---------------------------------------------------------------------------
+ 	
+var hammer = {first: 'Hammer', second: true, 
+ 			run: function(){
+ 				bag.push({name: 'Hammer', health: 0, integrity: 0, attack: 10});
+ 			}
+ 	};
+ 	
+//---------------------------------------------------------------------------
+
+var knife = {first: 'knife', second: true, 
+ 			run: function(){
+ 				bag.push({name: 'knife', health: 0, integrity: 0, attack: 2});
+ 			}
+ 	};
+
+//---------------------------------------------------------------------------
+ 	
+var gun = {first: 'gun', second: true, 
+ 			run: function(){
+ 				bag.push({name: 'gun', health: -5, integrity: 0, attack: 15});
+ 			}
+ 	};
+ 	
+//---------------------------------------------------------------------------
+
+var staff = {first: 'staff', second: true, 
+ 			run: function(){
+ 				bag.push({name: 'staff', health: 0, integrity: 0, attack: 2});
+ 			}
+ 	};
+
+//---------------------------------------------------------------------------
+ 	
+var claws = {first: 'claws', second: true, 
+ 			run: function(){
+ 				bag.push({name: 'claws', health: -5, integrity: 0, attack: 15});
+ 			}
+ 	};
+
+///////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------
+///////////////////////////////////////////////////////////////////
+
+
+
+	
+var weapons = []; 	
+ 	
+function pushw() {
+	weapons.push(sword);
+	weapons.push(hammer);
+	weapons.push(knife);
+	weapons.push(gun);
+	weapons.push(staff);
+	weapons.push(claws);
+}	
+
+pushw();
+/-------------------------------------------------------------------
+
+
+
+
+
+//var weapons = [sword]; 	
+//////////////////////////////////////////////////////////////////
  //---------------------------------------------------------------
- var menu = {
+  var menu;
+ /*
+ var town = {
  	on: false,
  	state: {info:true,prompt:false,buy:false,sell:false,sleep:false},
  	name: 'town name',
@@ -33,14 +116,22 @@ var context = canvas.getContext('2d');
  		}},
  	],
  	buy:[
- 		sword,
  		{first: 'sun cloak', second: false},
  		{first: 'running shoes', second: false}
  	],
  	leave: {no:true, yes: false}
  };
- 
+ */
+
  //-----------------------------------------------------------------
+ function initilize() {
+ 	makeTown();
+ }
+ 
+ 
+ 
+ 
+ 
  function draw(){
  	canvas.width = canvas.width;
  	console.log(menu.on);
@@ -62,10 +153,9 @@ var context = canvas.getContext('2d');
     //prints name of menu in top left corner
  	context.fillText(menu.name, 100, 200);
  	
+ 	printBag();
+ 	//----------------------------------------------------------------------------------
  	
- 	context.fillText('bag', 400, 150);
- 	if(bag[0] != null)context.fillText(bag[0].name, 400, 200);
- 	if(bag[1] != null)context.fillText(bag[1].name, 400, 250);
  	//this is the default info screen
  	//---------------------------------------
  	if(menu.state.info){
@@ -82,6 +172,7 @@ var context = canvas.getContext('2d');
  		//if more than five it will wrap to the right 
  		
  		printList(menu.options);
+ 		ontext.fillText("e (EXIT)",350,400);
  	//--------------------------------------	
  	//this creats the list of objects you can by in the shop	
  	}else if(menu.state.buy){
@@ -91,11 +182,12 @@ var context = canvas.getContext('2d');
  		//this prints out the item choices 50 spaces apart 
  		//if more than five it will wrap to the right 
  		printList(menu.buy);	
+ 		ontext.fillText("e (EXIT)",350,400);
  	}
  	//---------------------------------------
  }
  
- 
+ initilize();
  function gameLoop(){
  	draw();
  }
@@ -126,6 +218,16 @@ var context = canvas.getContext('2d');
        	  scanDown(menu.buy);
        }
     }
+//---------------------------------------------------------
+});
+
+addEventListener("keydown", function(key){
+ //-------------------------------------------------------
+ //lets you move up options
+    if(key.keyCode == "69"){
+    	//this allows you to exit from the menu
+        console.log('exit'); //e = "exit";   
+       
 //---------------------------------------------------------
 });
 
@@ -180,9 +282,11 @@ function printList(list) {
 			context.fillText(list[i].first,100 + locx,250 + locy);
 		}
 		// loops after 5
-		if(i%5 == 0 && i != 0) {
+		if((i+1)%5 == 0 && i != 0) {
+			console.log('DIVISIBLE BY FIVE');
+			console.log(locx);
 			locx += 50;
-			locy = 50;
+			locy = 0;
 		}
 		locy += 50;
 	}	
@@ -216,3 +320,80 @@ function scanUp(list){
 	}
 }
 //--------------------------------------------------------------------------
+
+function makeTown() {
+	menu = {
+ 	on: false,
+ 	state: {info:true,prompt:false,buy:false,sell:false,sleep:false},
+ 	name: 'town name',
+ 	info: 'info about the town',
+ 	prompt: 'Do you want to...',
+ 	shop: 'SHOP',
+ 	options: [
+ 		{first: 'buy', second: true, run: function(){
+ 			menu.state.prompt = false;
+	        menu.state.buy = true;
+ 		}},
+ 		{first: 'sell', second: false,run: function(){
+ 			menu.state.prompt = false;
+	        menu.state.sell = true;
+ 		}},
+ 		{first: 'sleep', second: false, run: function(){
+ 			menu.state.prompt = false;
+	        menu.state.sleep = true;
+ 		}},
+ 	],
+ 	buy:[],
+ 	leave: {no:true, yes: false}
+ };
+	
+	for(var i = 0; i < weapons.length; ++i) {
+		if(i == 0){
+			weapons[i].second = true;
+			menu.buy.push(weapons[i]); 
+		} else if(i > 0){
+			weapons[i].second = false;
+			menu.buy.push(weapons[i]); 
+		}
+	}
+}
+
+
+
+//this is function is mostly like the printlist but cannot scroll and 
+//is offset to right
+
+//////////////////////////////////////////////////////////////////////
+function printBag(){
+	context.fillText('bag', 400, 150);
+ 	//if(bag[0] != null)context.fillText(bag[0].name, 400, 200);
+ 	//if(bag[1] != null)context.fillText(bag[1].name, 400, 250);
+ 	
+ 	var locax = 0;
+ 	var locay = 50;
+ 	
+ 	for(var i = 0; i <bag.length;++i){
+ 		
+ 		/*if(bag[0] != null)*/
+ 		context.fillText(bag[i].name, 400 + locax, 200 + locay);
+ 		if((i+1)%5 == 0 && i != 0) {
+			console.log('DIVISIBLE BY FIVE');
+			console.log(locax);
+			locax += 50;
+			locay = 0;
+		}
+		locay += 50;
+ 		
+ 	}
+}
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
